c3571d487a0e32b53b94b9be2b5c7c970114fd58
ftamtoro <sjcftamt@sjca5ftampro.sjc.flextronics.com>
2017-03-17 13:16:11 -0700

    5

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -111,6 +111,29 @@ ParseRequest.getData = function() {
 
     }
 
+    // Create the table
+    var body = document.getElementsByTagName('body')[0];
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < 3; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < 2; j++) {
+            if (i == 2 && j == 1) {
+                break
+            } else {
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode('\u0020'))
+                i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+                tr.appendChild(td)
+            }
+        }
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl);
+
     // Limit to the last 5 items
     if (milliseconds.length > 5)
     {
